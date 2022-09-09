4bcc6c8b681457d705f7cfe901124b6edfa7935b
elenamoglan <moglan.elena@gmail.com>
2022-09-09 21:43:15 +0300

    added 11.js

---
+++ js-arrays/11.js
@@ -0,0 +1,6 @@
+const colors = ["white", "blue", "yellow", "black", "red", "green"];
+const [firstColor, secondColor, ...otherColors] = colors;
+
+console.log(firstColor);
+console.log(secondColor);
+console.log(otherColors);
