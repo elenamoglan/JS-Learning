ccf0c411cdc36159c7d9659f65af5c8aadf0c329
elenamoglan <moglan.elena@gmail.com>
2022-09-09 21:38:44 +0300

    added 6.js

---
+++ js-fundamentals/6.js
@@ -0,0 +1,13 @@
+let a = prompt('Insert the number');
+if (a < 0) {
+    alert('-1');
+}
+else if (a > 0) {
+    alert('1');
+}
+else if (a == 0) {
+    alert('0');
+}
+else {
+    alert('Error');
+}
