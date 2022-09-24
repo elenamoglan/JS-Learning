490b393ab8e5c59b8f8e55f8ff3d05f4496aafa5
elenamoglan <moglan.elena@gmail.com>
2022-09-24 13:41:34 +0300

    deleted else

--- DOM/6.js
+++ DOM/6.js
@@ -4,10 +4,9 @@ for (i = 1; i <= 8; i++) {
   let tr = document.createElement("tr");
   for (j = 1; j <= 8; j++) {
     let td = document.createElement("td");
+    td.style.backgroundColor = "white";
     if ((i + j) % 2 != 0) {
       td.style.backgroundColor = "black";
-    } else {
-      td.style.backgroundColor = "white";
     }
     tr.append(td);
   }
