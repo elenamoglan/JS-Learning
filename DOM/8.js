0e6a56f542a0c335375d0e53dc6d12f979c20f1e
elenamoglan <moglan.elena@gmail.com>
2022-09-24 13:57:11 +0300

    added event

--- DOM/8.js
+++ DOM/8.js
@@ -2,22 +2,18 @@ let input = document.getElementById("input");
 const add = document.getElementById("button");
 const list = document.getElementById("list");
 
-add.onclick = () => {
+add.addEventListener("click", () => {
   let li = document.createElement("li");
   li.textContent = input.value;
-  li.onclick = () => {
-    alert(li.textContent);
-  };
   list.append(li);
+  li.addEventListener("click", () => {
+    alert(li.textContent);
+  });
   input.value = "";
   input.focus();
   add.disabled = true;
-};
+});
 
-input.oninput = () => {
-  if (input.value != "") {
-    add.disabled = false;
-  } else {
-    add.disabled = true;
-  }
-};
+input.addEventListener("input", () => {
+  add.disabled = !input.value;
+});
