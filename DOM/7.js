ce7f9767d35706c787e3443e82fc54d1592863e1
elenamoglan <moglan.elena@gmail.com>
2022-09-24 12:33:50 +0300

    changed let to const

--- DOM/7.js
+++ DOM/7.js
@@ -1,6 +1,7 @@
-var links = document.getElementsByTagName("a");
-for (let link of links) {
-  let href = link.getAttribute("href");
+const links = document.getElementsByTagName("a");
+
+for (const link of links) {
+  const href = link.getAttribute("href");
 
   if (href.includes("https://")) {
     link.style.color = "red";
