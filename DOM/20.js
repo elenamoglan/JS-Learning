7901662c359e5137378117a97a040b6d05f1d1e9
elenamoglan <moglan.elena@gmail.com>
2022-10-03 17:01:00 +0300

    deleted function

--- DOM/20.js
+++ DOM/20.js
@@ -1,17 +1,18 @@
-const add_button = document.getElementById("add_button");
+const addButton = document.getElementById("add_button");
 const table = document.getElementById("table");
 const newRow = document.getElementById("newRow");
 
-table.addEventListener("click", function (event) {
+table.addEventListener("click", (event) => {
   if (event.target.closest(".delete_button")) {
     event.target.closest("tr").remove();
   }
 });
 
-add_button.addEventListener("click", () => {
+addButton.addEventListener("click", () => {
   const row = document.createElement("tr");
   const firstCell = document.createElement("td");
   const secondCell = document.createElement("td");
+
   firstCell.textContent = newRow.value;
   secondCell.setAttribute("class", "delete_button");
   secondCell.textContent = "X";
