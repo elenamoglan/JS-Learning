70784a1a8ff0f79fa0bbfc4c513720520274a45a
elenamoglan <moglan.elena@gmail.com>
2022-10-03 17:00:35 +0300

    changed variables

--- DOM/17.js
+++ DOM/17.js
@@ -1,12 +1,10 @@
 const element = document.getElementById("elem");
 
 element.addEventListener("click", () => {
-  alert(
-    `${getComputedStyle(element).height}, ${getComputedStyle(element).width}`
-  );
+  const { height, width } = getComputedStyle(element);
+  alert(`${height}, ${width}`);
 
-  const startWidth = element.offsetWidth;
-  const startHeight = element.offsetHeight;
-  element.style.width = `${startWidth * 2}px`;
-  element.style.height = `${startHeight * 2}px`;
+  const { offsetWidth, offsetHeight } = element;
+  element.style.width = `${offsetWidth * 2}px`;
+  element.style.height = `${offsetHeight * 2}px`;
 });
