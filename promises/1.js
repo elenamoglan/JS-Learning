f8cc8aac6a86a1ce72caf1ae206948696f964e23
elenamoglan <moglan.elena@gmail.com>
2022-11-07 19:33:42 +0200

    changed functions

--- promises/1.js
+++ promises/1.js
@@ -1,17 +1,16 @@
 function serverResponse(result) {
-  let promise = new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => resolve("Response from server"), 2000);
   });
-  promise.then(
-    (res) => (result.innerHTML = res),
-    (error) => (result.innerHTML = error)
-  );
 }
 
 function userRequest() {
   const result = document.getElementById("result");
   result.innerHTML = "Waiting for response";
-  serverResponse(result);
+  serverResponse(result).then(
+    (res) => (result.innerHTML = res),
+    (error) => (result.innerHTML = error)
+  );
 }
 
 userRequest();
