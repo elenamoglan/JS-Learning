65c4162d932edfe6b193edee927fbcba71e14361
elenamoglan <elenam.moglan@gmail.com>
2022-11-20 12:47:26 +0200

    changed innerHTML

--- local_storage/2.js
+++ local_storage/2.js
@@ -2,40 +2,42 @@ const tasks = document.getElementById("totalTasks");
 const addTask = document.getElementById("addTask");
 const list = document.getElementById("tasksList");
 
-let itemsArray = localStorage.getItem("items")
+const itemsArray = localStorage.getItem("items")
   ? JSON.parse(localStorage.getItem("items"))
   : [];
 
-tasks.innerHTML = `${itemsArray.length} tasks`;
+tasks.textContent = `${itemsArray.length} tasks`;
 
 function createTask(text) {
   const deleteButton = document.createElement("span");
   const task = document.createElement("p");
-  task.innerHTML = text;
-  deleteButton.innerHTML = "X";
+
+  task.textContent = text;
+  deleteButton.textContent = "X";
   deleteButton.setAttribute("item", text);
-  deleteButton.addEventListener("click", (event) => {
-    const targetElement = event.target;
+
+  deleteButton.addEventListener("click", ({target}) => {
     itemsArray.splice(
-      itemsArray.indexOf(targetElement.getAttribute("item")),
+      itemsArray.indexOf(target.getAttribute("item")),
       1
     );
     localStorage.setItem("items", JSON.stringify(itemsArray));
     list.removeChild(task);
-    tasks.innerHTML = `${itemsArray.length} tasks`;
+    tasks.textContent = `${itemsArray.length} tasks`;
   });
+
   task.append(deleteButton);
   list.append(task);
 }
 
-addTask.addEventListener("keypress", function (event) {
+addTask.addEventListener("keypress",  (event) => {
   if (event.key === "Enter") {
     event.preventDefault();
 
     itemsArray.push(addTask.value);
     localStorage.setItem("items", JSON.stringify(itemsArray));
     createTask(addTask.value);
-    tasks.innerHTML = `${itemsArray.length} tasks`;
+    tasks.textContent = `${itemsArray.length} tasks`;
     addTask.value = "";
   }
 });
